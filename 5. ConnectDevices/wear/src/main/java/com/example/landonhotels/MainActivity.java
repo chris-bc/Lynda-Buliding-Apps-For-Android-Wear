7d44719171005ff358e14842eb3263491ce29f0e
Chris Bennetts-Cash <chris@bennettscash.id.au>
2016-05-02 06:35:48 +1000

    Connect to google APIs

--- 5. ConnectDevices/wear/src/main/java/com/example/landonhotels/MainActivity.java
+++ 5. ConnectDevices/wear/src/main/java/com/example/landonhotels/MainActivity.java
@@ -14,7 +14,8 @@ import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.NodeApi;
 import com.google.android.gms.wearable.Wearable;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     public static final String WEARABLE_MAIN = "WearableMain";
 
@@ -28,12 +29,58 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main_layout);
 
         //Initialize mGoogleApiClient
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
 
     }
 
     public void buttonClickHandler(View view) {
 
         //Send message to handheld device
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Wearable.NodeApi.getConnectedNodes(mGoogleApiClient)
+                .setResultCallback(new ResultCallback<NodeApi.GetConnectedNodesResult>() {
+                    @Override
+                    public void onResult(NodeApi.GetConnectedNodesResult nodes) {
+                        for (Node node : nodes.getNodes()) {
+                            if (node != null && node.isNearby()) {
+                                mNode = node;
+                                Log.d(WEARABLE_MAIN, "Connected to " + node.getDisplayName());
+                            }
+                        }
+                        if (mNode == null) {
+                            Log.d(WEARABLE_MAIN, "Not connected");
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
 
     }
 }
