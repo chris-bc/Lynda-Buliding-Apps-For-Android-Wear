c605c9b1e84bba730e3e73efa94c59bb72f235a4
Chris Bennetts-Cash <chris@bennettscash.id.au>
2016-05-01 22:06:44 +1000

    Always on screen

--- 4. AlwaysOn/wear/src/main/java/com/example/android/landonhotels/MainActivity.java
+++ 4. AlwaysOn/wear/src/main/java/com/example/android/landonhotels/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.android.landonhotels;
 
-import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends WearableActivity {
 
     private TextView mTextView;
     private RelativeLayout mLayoutContainer;
@@ -15,8 +16,28 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setAmbientEnabled();
+
         mTextView = (TextView) findViewById(R.id.textView);
         mLayoutContainer = (RelativeLayout) findViewById(R.id.layoutContainer);
 
+    }
+
+    @Override
+    public void onEnterAnimationComplete() {
+        super.onEnterAnimationComplete();
+
+        mLayoutContainer.setBackgroundResource(android.R.color.black);
+        mTextView.setTextColor(Color.WHITE);
+        mTextView.getPaint().setAntiAlias(true);
+    }
+
+    @Override
+    public void onExitAmbient() {
+        super.onExitAmbient();
+
+        mLayoutContainer.setBackgroundResource(android.R.color.holo_blue_light);
+        mTextView.setTextColor(Color.BLACK);
+        mTextView.getPaint().setAntiAlias(false);
     }
 }
