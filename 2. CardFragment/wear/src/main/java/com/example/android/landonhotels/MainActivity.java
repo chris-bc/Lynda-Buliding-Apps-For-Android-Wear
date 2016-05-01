4c8fb3d05922a024e87492b4df3c0dc1616f84d4
Chris Bennetts-Cash <chris@bennettscash.id.au>
2016-05-01 21:05:43 +1000

    Basic card layout

--- 2. CardFragment/wear/src/main/java/com/example/android/landonhotels/MainActivity.java
+++ 2. CardFragment/wear/src/main/java/com/example/android/landonhotels/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.landonhotels;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.wearable.view.CardFragment;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 
@@ -13,12 +16,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-            }
-        });
+
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        CardFragment fragment = CardFragment.create(getString(R.string.card_title),
+                getString(R.string.card_description),
+                R.drawable.landon_icon);
+        transaction.add(R.id.frame_layout, fragment);
+        transaction.commit();
     }
 }
