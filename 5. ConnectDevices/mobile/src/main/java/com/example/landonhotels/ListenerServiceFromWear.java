df18e4e7d105e4f0b6764e3089bf2b9b4886ce0d
Chris Bennetts-Cash <chris@bennettscash.id.au>
2016-05-02 07:09:12 +1000

    Receive messages on mobile

--- 5. ConnectDevices/mobile/src/main/java/com/example/landonhotels/ListenerServiceFromWear.java
+++ 5. ConnectDevices/mobile/src/main/java/com/example/landonhotels/ListenerServiceFromWear.java
@@ -1,7 +1,24 @@
 package com.example.landonhotels;
 
-public class ListenerServiceFromWear {
+import android.content.Intent;
 
-    private static final String wearpath = "/from-wear";
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.WearableListenerService;
 
+public class ListenerServiceFromWear extends WearableListenerService{
+
+    private static final String WEAR_PATH = "/from-wear";
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        super.onMessageReceived(messageEvent);
+
+        if (messageEvent.getPath().equals(WEAR_PATH)) {
+            String city = new String(messageEvent.getData());
+            Intent i = new Intent(this, DetailActivity.class);
+            i.putExtra("city", city);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+        }
+    }
 }
