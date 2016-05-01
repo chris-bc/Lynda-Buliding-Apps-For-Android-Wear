a9986556c554d9511b2e07bd319b53b2346e7d2f
Chris Bennetts-Cash <chris@bennettscash.id.au>
2016-05-01 21:46:02 +1000

    Undo button

--- 3. Confirmation/wear/src/main/java/com/example/android/landonhotels/MainActivity.java
+++ 3. Confirmation/wear/src/main/java/com/example/android/landonhotels/MainActivity.java
@@ -1,25 +1,37 @@
 package com.example.android.landonhotels;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.wearable.activity.ConfirmationActivity;
+import android.support.wearable.view.DelayedConfirmationView;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements DelayedConfirmationView.DelayedConfirmationListener {
     private boolean isRunning = false;
+    private DelayedConfirmationView delayedConfirmationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        delayedConfirmationView = (DelayedConfirmationView)findViewById(R.id.delayed_confirm);
+        delayedConfirmationView.setListener(this);
+        delayedConfirmationView.setTotalTimeMs(3000);
+
         RelativeLayout container = (RelativeLayout) findViewById(R.id.container);
         container.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 //                Start the timer here
+                if (!isRunning) {
+                    showTimer(true);
+                    delayedConfirmationView.start();
+                    isRunning = true;
+                }
             }
         });
     }
@@ -35,5 +47,27 @@ public class MainActivity extends Activity {
             layout.setVisibility(View.INVISIBLE);
             textView.setVisibility(View.VISIBLE);
         }
+    }
+
+    @Override
+    public void onTimerFinished(View view) {
+        showTimer(false);
+        isRunning = false;
+        showConfirmation(ConfirmationActivity.SUCCESS_ANIMATION, "Confirmed");
+    }
+
+    private void showConfirmation(int animation, String message) {
+        Intent i = new Intent(this, ConfirmationActivity.class);
+        i.putExtra(ConfirmationActivity.EXTRA_ANIMATION_TYPE, animation);
+        i.putExtra(ConfirmationActivity.EXTRA_MESSAGE, message);
+        startActivity(i);
+    }
+
+    @Override
+    public void onTimerSelected(View view) {
+        showTimer(false);
+        isRunning = false;
+        delayedConfirmationView.reset();
+        showConfirmation(ConfirmationActivity.FAILURE_ANIMATION, "Cancelled");
     }
 }
